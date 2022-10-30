382041ee246bae794f6543528366418aa170599c
lukling0524 <59614692+lukling0524@users.noreply.github.com>
2022-10-30 10:28:41 +0900

    스타일 덮어쓰기

--- nepp-movie/src/App.js
+++ nepp-movie/src/App.js
@@ -1,11 +1,48 @@
+import { Link, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 import { Reset } from "styled-reset";
+import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Reset />
+      <AppBlock>
+        <Reset />
+        <ul>
+          <li>
+            <StyledLink to="/">HOME</StyledLink>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </AppBlock>
     </>
   );
 }
+
+const AppBlock = styled.div`
+  a {
+    /* color: inherit; */
+    text-decoration: none;
+    font-weight: 800;
+    font-size: 20px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+//스타일 덮어쓰기
+const StyledLink = styled(Link)`
+  color: red;
+`;
 
 export default App;
---
+++ nepp-movie/src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>About 컴포넌트임</p>
+    </div>
+  );
+}
+
+export default About;
